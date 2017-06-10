50468683a1293320a0602164549803b49a8b9808
yaron2 <schneider.yaron@live.com>
2017-06-10 19:05:30 +0300

    fixed paths

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+mkdir -p /var/lib/drill
+chmod -R 777 /var/lib/drill/
+cd /var/lib/drill
+
 wget http://apache.mirrors.hoobly.com/drill/drill-1.10.0/apache-drill-1.10.0.tar.gz
 tar -xzvf apache-drill-1.10.0.tar.gz
 
@@ -8,7 +12,6 @@ if [ -z "$ZKHOSTS" ]; then
     ZKHOSTS=`grep -R zk /etc/hadoop/conf/yarn-site.xml | grep 2181 | grep -oPm1 "(?<=<value>)[^<]+"`
 fi
 
-cp /etc/hadoop/conf/core-site.xml $HOME/apache-drill-1.10.0/conf/core-site.xml
-
 sed -i "s@localhost:2181@$ZKHOSTS@" apache-drill-1.10.0/conf/drill-override.conf
-$HOME/apache-drill-1.10.0/bin/drillbit.sh restart
+cp /etc/hadoop/conf/core-site.xml apache-drill-1.10.0/conf/core-site.xml
+apache-drill-1.10.0/bin/drillbit.sh restart

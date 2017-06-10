0f65cbbb9b4e9aca9ad4f39eaf977bb9f1994f7c
yaron2 <schneider.yaron@live.com>
2017-06-10 19:27:27 +0300

    fixed jar path issue

--- setup.sh
+++ setup.sh
@@ -2,18 +2,19 @@
 
 mkdir -p /var/lib/drill
 chmod -R 777 /var/lib/drill/
-cd /var/lib/drill
-
-mkdir -p jars/3rdparty
-
-cd jars/3rdparty/
-sudo wget http://central.maven.org/maven2/org/apache/hadoop/hadoop-azure/2.7.1/hadoop-azure-2.7.1.jar
-sudo wget http://central.maven.org/maven2/com/microsoft/azure/azure-storage/2.0.0/azure-storage-2.0.0.jar
 
 cd /var/lib/drill
 
 wget http://apache.mirrors.hoobly.com/drill/drill-1.10.0/apache-drill-1.10.0.tar.gz
 tar -xzvf apache-drill-1.10.0.tar.gz
+
+mkdir -p /var/lib/drill/apache-drill-1.10.0/jars/3rdparty
+cd /var/lib/drill/apache-drill-1.10.0/jars/3rdparty
+
+sudo wget http://central.maven.org/maven2/org/apache/hadoop/hadoop-azure/2.7.1/hadoop-azure-2.7.1.jar
+sudo wget http://central.maven.org/maven2/com/microsoft/azure/azure-storage/2.0.0/azure-storage-2.0.0.jar
+
+cd /var/lib/drill
 
 ZKHOSTS=`grep -R zookeeper /etc/hadoop/conf/yarn-site.xml | grep 2181 | grep -oPm1 "(?<=<value>)[^<]+"`
 if [ -z "$ZKHOSTS" ]; then

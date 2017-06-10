6fb35bccbcb7cfdd61c372fe104d218204149085
yaron2 <schneider.yaron@live.com>
2017-06-10 19:14:07 +0300

    added azure-storage jars

--- setup.sh
+++ setup.sh
@@ -4,6 +4,12 @@ mkdir -p /var/lib/drill
 chmod -R 777 /var/lib/drill/
 cd /var/lib/drill
 
+mkdir -p jars/3rdparty
+
+cd jars/3rdparty/
+sudo wget http://central.maven.org/maven2/org/apache/hadoop/hadoop-azure/2.7.1/hadoop-azure-2.7.1.jar
+sudo wget http://central.maven.org/maven2/com/microsoft/azure/azure-storage/2.0.0/azure-storage-2.0.0.jar
+
 wget http://apache.mirrors.hoobly.com/drill/drill-1.10.0/apache-drill-1.10.0.tar.gz
 tar -xzvf apache-drill-1.10.0.tar.gz
 

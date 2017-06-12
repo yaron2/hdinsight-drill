bd7ac8aa938339f59e6bdd8ca98032f53ba60f99
yaron2 <schneider.yaron@live.com>
2017-06-12 23:44:24 +0300

    fixed dir download path

--- setup.sh
+++ setup.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+VERSION=1.10.0
+
+if [[ -n $1 ]]; then
+    VERSION=$1
+fi    
+
 mkdir -p /var/lib/drill
 chmod -R 777 /var/lib/drill/
 
 cd /var/lib/drill
 
-wget http://apache.mirrors.hoobly.com/drill/drill-1.10.0/apache-drill-1.10.0.tar.gz
-tar -xzvf apache-drill-latest.tar.gz
+wget "http://apache.mirrors.hoobly.com/drill/drill-"$VERSION/apache-drill-"$VERSION.tar.gz"""
 
-mkdir -p /var/lib/drill/apache-drill-latest/jars/3rdparty
-cd /var/lib/drill/apache-drill-latest/jars/3rdparty
+DRILLDIR="apache-drill-$VERSION"
+
+tar -xzvf $DRILLDIR.tar.gz
+
+mkdir -p /var/lib/drill/$DRILLDIR/jars/3rdparty
+cd /var/lib/drill/$DRILLDIR/jars/3rdparty
 
 sudo wget http://central.maven.org/maven2/org/apache/hadoop/hadoop-azure/2.7.1/hadoop-azure-2.7.1.jar
 sudo wget http://central.maven.org/maven2/com/microsoft/azure/azure-storage/2.0.0/azure-storage-2.0.0.jar
@@ -21,6 +30,6 @@ if [ -z "$ZKHOSTS" ]; then
     ZKHOSTS=`grep -R zk /etc/hadoop/conf/yarn-site.xml | grep 2181 | grep -oPm1 "(?<=<value>)[^<]+"`
 fi
 
-sed -i "s@localhost:2181@$ZKHOSTS@" apache-drill-latest/conf/drill-override.conf
-ln -s /etc/hadoop/conf/core-site.xml apache-drill-latest/conf/core-site.xml
-apache-drill-latest/bin/drillbit.sh autorestart
+sed -i "s@localhost:2181@$ZKHOSTS@" $DRILLDIR/conf/drill-override.conf
+ln -s /etc/hadoop/conf/core-site.xml $DRILLDIR/conf/core-site.xml
+$DRILLDIR/bin/drillbit.sh restart

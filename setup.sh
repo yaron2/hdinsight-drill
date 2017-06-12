1f5ee765596ac770610af85574db5baacb48eafb
yaron2 <schneider.yaron@live.com>
2017-06-12 21:56:59 +0300

    changed copy to symlink, updated version handling

--- setup.sh
+++ setup.sh
@@ -6,10 +6,10 @@ chmod -R 777 /var/lib/drill/
 cd /var/lib/drill
 
 wget http://apache.mirrors.hoobly.com/drill/drill-1.10.0/apache-drill-1.10.0.tar.gz
-tar -xzvf apache-drill-1.10.0.tar.gz
+tar -xzvf apache-drill-latest.tar.gz
 
-mkdir -p /var/lib/drill/apache-drill-1.10.0/jars/3rdparty
-cd /var/lib/drill/apache-drill-1.10.0/jars/3rdparty
+mkdir -p /var/lib/drill/apache-drill-latest/jars/3rdparty
+cd /var/lib/drill/apache-drill-latest/jars/3rdparty
 
 sudo wget http://central.maven.org/maven2/org/apache/hadoop/hadoop-azure/2.7.1/hadoop-azure-2.7.1.jar
 sudo wget http://central.maven.org/maven2/com/microsoft/azure/azure-storage/2.0.0/azure-storage-2.0.0.jar
@@ -21,6 +21,6 @@ if [ -z "$ZKHOSTS" ]; then
     ZKHOSTS=`grep -R zk /etc/hadoop/conf/yarn-site.xml | grep 2181 | grep -oPm1 "(?<=<value>)[^<]+"`
 fi
 
-sed -i "s@localhost:2181@$ZKHOSTS@" apache-drill-1.10.0/conf/drill-override.conf
-cp /etc/hadoop/conf/core-site.xml apache-drill-1.10.0/conf/core-site.xml
-apache-drill-1.10.0/bin/drillbit.sh restart
+sed -i "s@localhost:2181@$ZKHOSTS@" apache-drill-latest/conf/drill-override.conf
+ln -s /etc/hadoop/conf/core-site.xml apache-drill-latest/conf/core-site.xml
+apache-drill-latest/bin/drillbit.sh autorestart
